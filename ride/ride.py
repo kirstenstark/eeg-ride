ccfb10d2449925a985a2420c0754b0da2a30df51
Alexander Enge <alexander.enge@hu-berlin.de>
2023-04-18 11:46:47 +0200

    Add beginning of `ride_iter` incl. filtering

--- ride/ride.py
+++ ride/ride.py
@@ -2,6 +2,7 @@ from pyprojroot.here import here
 from scipy.io import savemat, loadmat
 from tests import generate_sine_data, plot_input_data
 import numpy as np
+from scipy.fft import fft, ifft
  
 # # Generate simulated sine data
 # data, rt = generate_sine_data()
@@ -66,3 +67,72 @@ for j in range(cfg['comp_num']):
     
     cfg['comp']['twd'][j] = np.fix((cfg['comp']['twd'][j] - cfg['epoch_twd'][0])/cfg['re_samp'])+[1,-1]
 
+
+stop = 1
+
+# TODO: Add "outer iteration" loop around here if there are one or more C components
+
+cfg1 = cfg.copy()
+cfg1['final'] = stop
+cfg1['inner_iter'] = 100
+
+c_l = np.zeros((d1, cfg['comp_num'], d2))
+c_sl = c_l
+
+for c in range(d2):
+    rst = ride_iter(np.squeeze(data[:, c, :]), cfg1)
+
+
+def ride_iter(data, cfg):
+
+    d1, d2 = data.shape
+
+    data0 = data.copy()
+
+    data = filtering20(data, 1, np.fix(10 * 20 * cfg['re_samp'] * d1 / 1000))
+    # TODO: Compare with MNE filter
+
+    max_latency = np.zeros((cfg['comp_num']))
+    min_latency = np.zeros((cfg['comp_num']))
+    length_c = np.zeros((cfg['comp_num']))
+    for j in range(cfg['comp_num']):
+        max_latency[j] = cfg['comp']['latency'][j].max()
+        min_latency[j] = cfg['comp']['latency'][j].min()
+        length_c[j] = d1 + max_latency[j] - min_latency[j]
+        
+    com_c = np.zeros((d1, cfg['comp_num']))
+    com_c1 = np.zeros((d1, d2, cfg['comp_num']))
+    amp_c = np.zeros((d2, cfg['comp_num']))
+
+    ### CONTINUE HERE ###
+
+
+def filtering20(x, a, b):
+
+    f = x
+    for j in range(x.shape[1]):
+        f[:, j] = filtering10(x[:, j], a, b)
+    
+    return f
+
+
+def filtering10(x, a, b):
+
+    # The MATLAB code does x = x(:) here but this doesn't seem to change anything
+    x0 = x.mean()
+    x = x - x0
+    n = 10 * len(x)
+    Y = fft(x, n)
+    
+    b = int(b)
+    H = np.concatenate([np.zeros((1, a)),
+                        np.expand_dims(Y[a : b + 1], axis=0),
+                        np.zeros((1, n - b * 2 - 1)),
+                        np.expand_dims(Y[n - b : n - a + 1], axis=0),
+                        np.zeros((1, a - 1))], axis=1)
+    H = H.transpose()
+
+    f = ifft(H, axis=0).real
+    f = np.squeeze(f)
+    
+    return f[:len(x)]
