6bdeadd9838db98f6e03ebb7d0b7d6460aede840
Alexander Enge <alexander.enge@hu-berlin.de>
2023-05-16 08:41:34 +0200

    Move functions up for easier execution

--- ride/ride.py
+++ ride/ride.py
@@ -2,7 +2,7 @@ import numpy as np
 from pyprojroot.here import here
 from scipy.fft import fft, ifft
 from scipy.io import loadmat, savemat
-from tests import generate_sine_data, plot_input_data
+# from tests import generate_sine_data, plot_input_data
 
 # # Generate simulated sine data
 # data, rt = generate_sine_data()
@@ -85,9 +85,43 @@ c_sl = c_l
 #     rst = ride_iter(np.squeeze(data[:, c, :]), cfg1)
 
 
+def filtering20(x, a, b):
+
+    f = x
+    for j in range(x.shape[1]):
+        f[:, j] = filtering10(x[:, j], a, b)
+
+    return f
+
+
+def filtering10(x, a, b):
+
+    # The MATLAB code does x = x(:) here but this doesn't seem to change anything
+    x0 = x.mean()
+    x = x - x0
+    n = 10 * len(x)
+    Y = fft(x, n)
+
+    b = int(b)
+    H = np.concatenate([np.zeros((1, a)),
+                        np.expand_dims(Y[a: b + 1], axis=0),
+                        np.zeros((1, n - b * 2 - 1)),
+                        np.expand_dims(Y[n - b: n - a + 1], axis=0),
+                        np.zeros((1, a - 1))], axis=1)
+    H = H.transpose()
+
+    f = ifft(H, axis=0).real
+    f = np.squeeze(f)
+
+    return f[:len(x)]
+
+
 def ride_iter(data, cfg):
-    #cfg = cfg1
-    # data = data[:, 61, :]
+    
+    # These are the inputs that get passed to the function
+    # Remove these two lines once the function is complete!
+    cfg = cfg1
+    data = data[:, 61, :]
 
     d1, d2 = data.shape
 
@@ -158,34 +192,3 @@ def ride_iter(data, cfg):
                     temp[np.arange(-cfg['comp']['latency'][c][j]+max_latency[c],d1-cfg['comp']['latency'][c][j]+max_latency[c]),j] = residue[:,j]
 
                  ### CONTINUE HERE ###
-
-
-def filtering20(x, a, b):
-
-    f = x
-    for j in range(x.shape[1]):
-        f[:, j] = filtering10(x[:, j], a, b)
-
-    return f
-
-
-def filtering10(x, a, b):
-
-    # The MATLAB code does x = x(:) here but this doesn't seem to change anything
-    x0 = x.mean()
-    x = x - x0
-    n = 10 * len(x)
-    Y = fft(x, n)
-
-    b = int(b)
-    H = np.concatenate([np.zeros((1, a)),
-                        np.expand_dims(Y[a: b + 1], axis=0),
-                        np.zeros((1, n - b * 2 - 1)),
-                        np.expand_dims(Y[n - b: n - a + 1], axis=0),
-                        np.zeros((1, a - 1))], axis=1)
-    H = H.transpose()
-
-    f = ifft(H, axis=0).real
-    f = np.squeeze(f)
-
-    return f[:len(x)]
