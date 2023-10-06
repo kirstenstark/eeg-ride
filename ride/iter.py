c2ed613b5bf566ec39197518d791eb0224584cec
Alexander Enge <alexander.enge@hu-berlin.de>
2023-10-06 13:30:54 +0200

    Refactor ride with classes

--- ride/iter.py
+++ ride/iter.py
@@ -14,46 +14,46 @@ def ride_iter(data, cfg):
     # data = data[:, 61, :]
 
     d1, d2 = data.shape
-    bd = cfg['bd']
+    bd = cfg.bd
 
     data0 = data.copy()
 
-    data = filtering20(data, 1, np.fix(10 * 20 * cfg['re_samp'] * d1 / 1000))
+    data = filtering20(data, 1, np.fix(10 * 20 * cfg.re_samp * d1 / 1000))
     # TODO: Compare with MNE filter
 
-    max_latency = np.zeros((cfg['comp_num']), dtype=int)
-    min_latency = np.zeros((cfg['comp_num']), dtype=int)
-    length_c = np.zeros((cfg['comp_num']), dtype=int)
-    for j in range(cfg['comp_num']):
-        max_latency[j] = cfg['comp']['latency'][j].max()
-        min_latency[j] = cfg['comp']['latency'][j].min()
+    max_latency = np.zeros((cfg.comp_num), dtype=int)
+    min_latency = np.zeros((cfg.comp_num), dtype=int)
+    length_c = np.zeros((cfg.comp_num), dtype=int)
+    for j in range(cfg.comp_num):
+        max_latency[j] = cfg.comp_latency[j].max()
+        min_latency[j] = cfg.comp_latency[j].min()
         length_c[j] = d1 + max_latency[j] - min_latency[j]
 
-    com_c = np.zeros((d1, cfg['comp_num']))
-    com_c1 = np.zeros((d1, d2, cfg['comp_num']))
-    amp_c = np.zeros((d2, cfg['comp_num']))
+    com_c = np.zeros((d1, cfg.comp_num))
+    com_c1 = np.zeros((d1, d2, cfg.comp_num))
+    amp_c = np.zeros((d2, cfg.comp_num))
 
-    trend_i = cfg['comp_num']-1
-    stream_flow = np.arange(cfg['comp_num'])
-    for c in range(cfg['comp_num']):
-        if cfg['comp']['name'][c] == 'r':
+    trend_i = cfg.comp_num-1
+    stream_flow = np.arange(cfg.comp_num)
+    for c in range(cfg.comp_num):
+        if cfg.comp_name[c] == 'r':
             trend_i = c-1
-    if trend_i == cfg['comp_num']:
+    if trend_i == cfg.comp_num:
         stream_flow = np.concatenate([np.array([trend_i]), np.arange(trend_i)])
-    if trend_i == cfg['comp_num']-2 : 
-        stream_flow = np.concatenate([np.array([trend_i]), np.arange(trend_i), np.array([cfg['comp_num']-1])])
+    if trend_i == cfg.comp_num-2 : 
+        stream_flow = np.concatenate([np.array([trend_i]), np.arange(trend_i), np.array([cfg.comp_num-1])])
     if trend_i == 0 : 
         stream_flow = np.array([1,0])
 
-    l1 = np.zeros((cfg['inner_iter'], cfg['comp_num']))
+    l1 = np.zeros((cfg.inner_iter, cfg.comp_num))
     stop = 0
-    stop_c = np.zeros((cfg['comp_num'], 1))
+    stop_c = np.zeros((cfg.comp_num, 1))
     com_old = com_c.copy()
 
     # for iter in np.arange(1, 2): # For testing purposes, only runs iter = 1
-    for iter in np.arange(cfg['inner_iter']):
+    for iter in np.arange(cfg.inner_iter):
 
-        if iter + 1 == cfg['inner_iter']:
+        if iter + 1 == cfg.inner_iter:
             stop = 1
 
         # track the convergence
@@ -62,22 +62,22 @@ def ride_iter(data, cfg):
         # TO DO: add if-loop for iter > 1
 
         if iter > 1: 
-            for c in np.arange(cfg['comp_num']):
+            for c in np.arange(cfg.comp_num):
                 l1[iter-2,c]=np.sum(np.abs(com_c[:,c] - com_old[:,c]))
                 if iter > 2:
                     if l1[iter-3,c] - l1[iter-2,c] < 0.01*(l1[0,c] - l1[1,c]): # convergence is now defined as 0.01
                         stop_c[c] = 1
                     if l1[iter-3,c] == l1[iter-2,c]:
                         stop_c[c] = 1
-            if cfg['comp_num'] == 1:
+            if cfg.comp_num == 1:
                 stop_c[c] = 1
-        for c in np.arange(cfg['comp_num']):
+        for c in np.arange(cfg.comp_num):
             com_old[:,c] = com_c[:,c]
             # decision of the termination of the interation of each component
             
 
         
-        #for c in np.arange(cfg['comp_num']):
+        #for c in np.arange(cfg.comp_num):
             #com_old[:,c]=com_c[:,c] # decision of the termination of iteration of each component
         ## CAUTION: for-loop seems unnecessary
 
@@ -86,29 +86,29 @@ def ride_iter(data, cfg):
         for c in stream_flow:
             if stop_c[c] == 0:
                 temp = data.copy()
-                for j in np.arange(cfg['comp_num']):
+                for j in np.arange(cfg.comp_num):
                     if j != c:
                         temp = temp-com_c1[:, :, j]
                 residue = temp.copy()
                 temp = np.empty((length_c[c], d2))
                 temp[:] = np.nan
                 for j in np.arange(d2):
-                    temp[np.arange(-cfg['comp']['latency'][c][j]+max_latency[c],d1-cfg['comp']['latency'][c][j]+max_latency[c]),j] = residue[:,j]
+                    temp[np.arange(-cfg.comp_latency[c][j]+max_latency[c],d1-cfg.comp_latency[c][j]+max_latency[c]),j] = residue[:,j]
 
                 temp = ride_detrend(
                     temp,
                     np.array([0,
-                              np.fix((cfg['comp']['twd'][c][1] - cfg['comp']['twd'][c][0]) * bd),
-                              np.fix((cfg['comp']['twd'][c][1] - cfg['comp']['twd'][c][0]) * (1 - bd)) - 1,
-                              cfg['comp']['twd'][c][1] - cfg['comp']['twd'][c][0] - 1], dtype=int) \
-                                + max_latency[c] + int(cfg['comp']['twd'][c][0]))
+                              np.fix((cfg.comp_twd[c][1] - cfg.comp_twd[c][0]) * bd),
+                              np.fix((cfg.comp_twd[c][1] - cfg.comp_twd[c][0]) * (1 - bd)) - 1,
+                              cfg.comp_twd[c][1] - cfg.comp_twd[c][0] - 1], dtype=int) \
+                                + max_latency[c] + int(cfg.comp_twd[c][0]))
                 
                 temp0 = median_2d(temp.T)
-                temp0[np.concatenate([np.arange(cfg['comp']['twd'][c][0], dtype=int) + max_latency[c],
-                                      np.arange(cfg['comp']['twd'][c][1] + max_latency[c]-1, length_c[c], dtype=int)])]=0
-                temp0[np.arange(cfg['comp']['twd'][c][0]+ max_latency[c]-1, cfg['comp']['twd'][c][1] + max_latency[c], dtype=int)] = \
-                    temp0[np.arange(cfg['comp']['twd'][c][0]+ max_latency[c]-1, cfg['comp']['twd'][c][1] + max_latency[c], 
-                                    dtype=int)] * ride_tukey(cfg['comp']['twd'][c][1] - cfg['comp']['twd'][c][0] + 1, bd*2)
+                temp0[np.concatenate([np.arange(cfg.comp_twd[c][0], dtype=int) + max_latency[c],
+                                      np.arange(cfg.comp_twd[c][1] + max_latency[c]-1, length_c[c], dtype=int)])]=0
+                temp0[np.arange(cfg.comp_twd[c][0]+ max_latency[c]-1, cfg.comp_twd[c][1] + max_latency[c], dtype=int)] = \
+                    temp0[np.arange(cfg.comp_twd[c][0]+ max_latency[c]-1, cfg.comp_twd[c][1] + max_latency[c], 
+                                    dtype=int)] * ride_tukey(cfg.comp_twd[c][1] - cfg.comp_twd[c][0] + 1, bd*2)
      
                 temp1 = np.repeat(temp0[:,np.newaxis],d2, axis=1)
                 temp1[np.isnan(temp)] = np.nan
@@ -127,7 +127,7 @@ def ride_iter(data, cfg):
             for c in stream_flow:
 
                 temp = data.copy()
-                for j in np.arange(cfg['comp_num']):
+                for j in np.arange(cfg.comp_num):
                     if j != c:
                         temp = temp - com_c1[:,:,j]
 
@@ -136,25 +136,25 @@ def ride_iter(data, cfg):
                 temp[:] = np.nan
 
                 for j in np.arange(d2):
-                    temp[np.arange(-cfg['comp']['latency'][c][j]+max_latency[c],
-                                   d1-cfg['comp']['latency'][c][j]+max_latency[c]),
+                    temp[np.arange(-cfg.comp_latency[c][j]+max_latency[c],
+                                   d1-cfg.comp_latency[c][j]+max_latency[c]),
                          j] = residue[:,j]
 
-                if cfg['final'] == 1:
+                if cfg.final == 1:
                     temp[np.isnan(temp)] = 0
                     amp_c[:,c] = np.mean(
-                        np.repeat(com_c[np.arange(cfg["comp"]["twd"][c][0] - 1, cfg["comp"]["twd"][c][1], dtype=int), c, np.newaxis], d2, axis=1) * temp[max_latency[c] + np.arange(cfg["comp"]["twd"][c][0] - 1, cfg["comp"]["twd"][c][1], dtype=int), :], axis=0)
+                        np.repeat(com_c[np.arange(cfg.comp_twd[c][0] - 1, cfg.comp_twd[c][1], dtype=int), c, np.newaxis], d2, axis=1) * temp[max_latency[c] + np.arange(cfg.comp_twd[c][0] - 1, cfg.comp_twd[c][1], dtype=int), :], axis=0)
 
             break
         # end of inner iter loop
     
     # for last iteration
-    if cfg['final'] == 1:
+    if cfg.final == 1:
         # release time window function and detrending
         # allocate trend to the last C component
         for c in stream_flow:
             temp = data0.copy()
-            for j in np.arange(cfg['comp_num']):
+            for j in np.arange(cfg.comp_num):
                 if j != c:
                     temp = temp - com_c1[:,:,j]
                     # TODO: Probably only for microsaccades, temp-com_ms1 if 'ms' and 'var' exist
@@ -163,19 +163,19 @@ def ride_iter(data, cfg):
             temp[:] = np.nan
 
             for j in np.arange(d2):
-                temp[np.arange(-cfg['comp']['latency'][c][j]+max_latency[c],
-                               d1-cfg['comp']['latency'][c][j]+max_latency[c]),
+                temp[np.arange(-cfg.comp_latency[c][j]+max_latency[c],
+                               d1-cfg.comp_latency[c][j]+max_latency[c]),
                                j] = residue[:,j]
             temp0 = mean_nan(temp,1)
 
             if c==stream_flow[0]:
-                temp0[np.arange(cfg['comp']['twd'][c][0] + max_latency[c],dtype=int)]=0.0
-                tem = ride_tukey(cfg['comp']['twd'][c][1] - cfg['comp']['twd'][c][0] + 1, bd*2)
+                temp0[np.arange(cfg.comp_twd[c][0] + max_latency[c],dtype=int)]=0.0
+                tem = ride_tukey(cfg.comp_twd[c][1] - cfg.comp_twd[c][0] + 1, bd*2)
                 tem = tem[np.arange(np.fix(len(tem)/2).astype(int))]
-                temp0[np.arange(cfg['comp']['twd'][c][0] + max_latency[c]-1, 
-                                cfg['comp']['twd'][c][0] + max_latency[c] + len(tem)-1, dtype=int)] = \
-                                    temp0[np.arange(cfg['comp']['twd'][c][0] + max_latency[c]-1,\
-                                                    cfg['comp']['twd'][c][0] + max_latency[c] + len(tem)-1, dtype=int)] * tem
+                temp0[np.arange(cfg.comp_twd[c][0] + max_latency[c]-1, 
+                                cfg.comp_twd[c][0] + max_latency[c] + len(tem)-1, dtype=int)] = \
+                                    temp0[np.arange(cfg.comp_twd[c][0] + max_latency[c]-1,\
+                                                    cfg.comp_twd[c][0] + max_latency[c] + len(tem)-1, dtype=int)] * tem
                 
             temp1 = np.repeat(temp0[:,np.newaxis],d2, axis=1)
             temp1[np.isnan(temp)] = np.nan
@@ -185,7 +185,7 @@ def ride_iter(data, cfg):
         
         c = stream_flow[0]
         temp = data0.copy()
-        for j in np.arange(cfg['comp_num']):
+        for j in np.arange(cfg.comp_num):
             if j != c:
                 temp = temp - com_c1[:,:,j]
                 # TODO: Probably only for microsaccades, temp-com_ms1 if 'ms' and 'var' exist
@@ -193,8 +193,8 @@ def ride_iter(data, cfg):
         temp = np.empty((length_c[c], d2))
         temp[:] = np.nan
         for j in np.arange(d2):
-            temp[np.arange(-cfg['comp']['latency'][c][j]+max_latency[c],
-                           d1-cfg['comp']['latency'][c][j]+max_latency[c]),
+            temp[np.arange(-cfg.comp_latency[c][j]+max_latency[c],
+                           d1-cfg.comp_latency[c][j]+max_latency[c]),
                            j] = residue[:,j]
         temp0 = mean_nan(temp,1)
         temp1 = np.repeat(temp0[:,np.newaxis],d2, axis=1)
--- ride/ride.py
+++ ride/ride.py
@@ -4,6 +4,7 @@ from .baseline import baseline
 from .helpers import round_like_matlab
 from .interp2d import interp2d
 from .iter import ride_iter
+from .results import RideResults
 
 
 def ride_call(data, cfg):
@@ -18,7 +19,7 @@ def ride_call(data, cfg):
     # rt = rt.astype('float64')
 
     # start RIDE correction
-    assert len(cfg['comp']['name']) > 1, 'At least two components are required'
+    assert len(cfg.comp_name) > 1, 'At least two components are required'
 
     # TODO: make sure order is always s, (c), r
 
@@ -28,45 +29,44 @@ def ride_call(data, cfg):
     d1, d2, d3 = data.shape
     epoch_length = d1
     erp = data.mean(axis=2)
-    results = {'erp': erp,
-            'latency0': cfg['comp']['latency'].copy()}
+    results = RideResults(erp=erp, latency0=cfg.comp_latency.copy())
 
-    rs = cfg['re_samp'] / cfg['samp_interval']
+    rs = cfg.re_samp / cfg.samp_interval
     data = data[round_like_matlab(np.linspace(0, d1 - 1, int(d1 / rs))), :, :]
     d1, d2, d3 = data.shape  # New size after down samping
 
-    for j in range(cfg['comp_num']):
+    for j in range(cfg.comp_num):
 
-        if isinstance(cfg['comp']['latency'][j], int):
-            int_value = cfg['comp']['latency'][j]
+        if isinstance(cfg.comp_latency[j], int):
+            int_value = cfg.comp_latency[j]
             print(f'WARNING: Extending integer latency {int_value} to a vector of {int_value}s (one per trial)')
-            cfg['comp']['latency'][j] = np.array([[int_value]] * d3)
+            cfg.comp_latency[j] = np.array([[int_value]] * d3)
 
-        if cfg['comp']['name'][j] == 'r':
-            cfg['comp']['twd'][j] = cfg['comp']['twd'][j] + np.median(results['latency0'][j])
-            cfg['comp']['twd'][j][cfg['comp']['twd'][j] < cfg['rwd']] = cfg['rwd']
-            cfg['comp']['twd'][j][cfg['comp']['twd'][j] > cfg['epoch_twd'][1]] = cfg['epoch_twd'][1]
+        if cfg.comp_name[j] == 'r':
+            cfg.comp_twd[j] = cfg.comp_twd[j] + np.median(results.latency0[j])
+            cfg.comp_twd[j][cfg.comp_twd[j] < cfg.rwd] = cfg.rwd
+            cfg.comp_twd[j][cfg.comp_twd[j] > cfg.epoch_twd[1]] = cfg.epoch_twd[1]
 
-        cfg['comp']['latency'][j] = cfg['comp']['latency'][j] / cfg['re_samp']
-        cfg['comp']['latency'][j] = round_like_matlab(cfg['comp']['latency'][j]-np.median(cfg['comp']['latency'][j])) 
+        cfg.comp_latency[j] = cfg.comp_latency[j] / cfg.re_samp
+        cfg.comp_latency[j] = round_like_matlab(cfg.comp_latency[j]-np.median(cfg.comp_latency[j])) 
 
-        cfg['comp']['twd'][j] = np.fix((cfg['comp']['twd'][j] - cfg['epoch_twd'][0])/cfg['re_samp'])+[1, -1]
+        cfg.comp_twd[j] = np.fix((cfg.comp_twd[j] - cfg.epoch_twd[0])/cfg.re_samp)+[1, -1]
 
     stop = 1
 
     # TODO: Add "outer iteration" loop around here if there are one or more C components
 
     cfg1 = cfg.copy()
-    cfg1['final'] = stop
-    cfg1['inner_iter'] = 100
+    cfg1.final = stop
+    cfg1.inner_iter = 100
 
-    c_l = np.zeros((d1, cfg['comp_num'], d2))
+    c_l = np.zeros((d1, cfg.comp_num, d2))
     c_sl = c_l.copy()
 
-    amp = np.zeros((d3, d2, cfg['comp_num']))
+    amp = np.zeros((d3, d2, cfg.comp_num))
     for c in range(d2):
 
-        if cfg['prg'] == 1:
+        if cfg.prg == 1:
             print(f'Processing electrode #{c}')
 
         rst = ride_iter(np.squeeze(data[:, c, :]), cfg1)
@@ -77,23 +77,23 @@ def ride_call(data, cfg):
         if stop == 1:
             amp[:, c, :] = rst['amp']
         
-        if cfg['prg'] == 1:
+        if cfg.prg == 1:
             print(f'Took {rst["iter"] + 1} iterations')
 
     comp = c_l.transpose((0, 2, 1))
     comp1 = c_sl.transpose((0, 2, 1))
 
-    results['erp_new'] = 0
-    results['residue'] = erp
+    results.erp_new = 0
+    results.residue = erp
 
-    bl_wd = np.arange(-cfg['epoch_twd'][0]/cfg['samp_interval'],
-                    -cfg['epoch_twd'][0]/cfg['samp_interval']+cfg['bl']/cfg['samp_interval'],
+    bl_wd = np.arange(-cfg.epoch_twd[0]/cfg.samp_interval,
+                    -cfg.epoch_twd[0]/cfg.samp_interval+cfg.bl/cfg.samp_interval,
                     dtype=int)
 
-    component = np.zeros((d1, d2, cfg['comp_num']))
-    component1 = np.zeros((d1, d2, cfg['comp_num']))
+    component = np.zeros((d1, d2, cfg.comp_num))
+    component1 = np.zeros((d1, d2, cfg.comp_num))
 
-    for j in np.arange(cfg['comp_num']):
+    for j in np.arange(cfg.comp_num):
         # The MATLAB version explicitly requests "spline" as the interpolation method
         # Our Python function `interp2d` only performs this spline interpolation,
         # with no support for any other method (unlike the MATLAB function)
@@ -105,26 +105,26 @@ def ride_call(data, cfg):
                                         np.round(np.linspace(0, epoch_length, d1, endpoint=False)),
                                         np.arange(0, epoch_length))
         component1[:, :, j] = baseline(component1[:, :, j],bl_wd)
-        results['residue'] = results['residue'] - component1[:, :, j]
-        results[cfg['comp']['name'][j]] = component[:, :, j]
-        results[cfg['comp']['name'][j]+'_sl'] = component1[:, :, j]
-        results['latency_'+cfg['comp']['name'][j]] = cfg['comp']['latency'][j]*cfg['re_samp']
-        results['amp_'+cfg['comp']['name'][j]] = amp[:, :, j]
+        results.residue = results.residue - component1[:, :, j]
+        results.comps[cfg.comp_name[j]] = component[:, :, j]
+        results.comps_sl[cfg.comp_name[j]] = component1[:, :, j]
+        results.latencies[cfg.comp_name[j]] = cfg.comp_latency[j]*cfg.re_samp
+        results.amps[cfg.comp_name[j]] = amp[:, :, j]
 
-    results[cfg['comp']['name'][0]] = baseline(results[cfg['comp']['name'][0]],bl_wd) + np.repeat(np.mean(erp[bl_wd, :], axis=0)[np.newaxis, :], epoch_length, axis=0)
-    results[cfg['comp']['name'][0]+'_sl']= baseline(results[cfg['comp']['name'][0]+'_sl'],bl_wd) + np.repeat(np.mean(erp[bl_wd, :], axis=0)[np.newaxis, :], epoch_length, axis=0)
+    results.comps[cfg.comp_name[0]] = baseline(results.comps[cfg.comp_name[0]],bl_wd) + np.repeat(np.mean(erp[bl_wd, :], axis=0)[np.newaxis, :], epoch_length, axis=0)
+    results.comps_sl[cfg.comp_name[0]] = baseline(results.comps_sl[cfg.comp_name[0]],bl_wd) + np.repeat(np.mean(erp[bl_wd, :], axis=0)[np.newaxis, :], epoch_length, axis=0)
 
-    results[cfg['comp']['name'][rst['trend_c']]] = baseline(results[cfg['comp']['name'][rst['trend_c']]] + results['residue'],bl_wd)
-    results[cfg['comp']['name'][rst['trend_c']]+'_sl'] = baseline(results[cfg['comp']['name'][rst['trend_c']]+'_sl'] + results['residue'],bl_wd)
+    results.comps[cfg.comp_name[rst['trend_c']]] = baseline(results.comps[cfg.comp_name[rst['trend_c']]] + results.residue,bl_wd)
+    results.comps_sl[cfg.comp_name[rst['trend_c']]] = baseline(results.comps_sl[cfg.comp_name[rst['trend_c']]] + results.residue,bl_wd)
 
-    if  cfg['comp_num'] == 1:
-        bl_wd = np.arange(-cfg['epoch_twd'][0]/cfg['samp_interval'], dtype='int')
-        results[cfg['comp']['name'][0]] = baseline(results[cfg['comp']['name'][0]] + results['residue'],bl_wd) + np.repeat(np.mean(erp[bl_wd, :], axis=0)[np.newaxis, :], epoch_length, axis=0)
-        results[cfg['comp']['name'][0]+'_sl'] = baseline(results[cfg['comp']['name'][0]+'_sl'] + results['residue'],bl_wd) + np.repeat(np.mean(erp[bl_wd, :], axis=0)[np.newaxis, :], epoch_length, axis=0)
+    if  cfg.comp_num == 1:
+        bl_wd = np.arange(-cfg.epoch_twd[0]/cfg.samp_interval, dtype='int')
+        results.comps[cfg.comp_name[0]] = baseline(results.comps[cfg.comp_name[0]] + results.residue,bl_wd) + np.repeat(np.mean(erp[bl_wd, :], axis=0)[np.newaxis, :], epoch_length, axis=0)
+        results.comps_sl[cfg.comp_name[0]] = baseline(results.comps_sl[cfg.comp_name[0]] + results.residue,bl_wd) + np.repeat(np.mean(erp[bl_wd, :], axis=0)[np.newaxis, :], epoch_length, axis=0)
 
-    for j in np.arange(cfg['comp_num']):
-        results['erp_new'] = results['erp_new'] + results[cfg['comp']['name'][j]]
+    for j in np.arange(cfg.comp_num):
+        results.erp_new = results.erp_new + results.comps[cfg.comp_name[j]]
 
-    results['cfg'] = cfg0.copy()
+    results.cfg = cfg0.copy()
 
     return results
