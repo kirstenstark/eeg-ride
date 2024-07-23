d760fa8e64e16d9edb1473db2a311ccbff30da8b
Alexander Enge <alexander.enge@hu-berlin.de>
2024-07-23 11:21:37 +0200

    Export package version

--- ride/__init__.py
+++ ride/__init__.py
@@ -1,3 +1,11 @@
 from .ride import ride_call
 from .cfg import RideCfg
 from .correct import correct_trials
+
+# Get current package version
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
